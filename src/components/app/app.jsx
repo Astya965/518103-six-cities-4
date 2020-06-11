3ae4948b6e068ba94636996d7dc4dde376d0b212
astya965 <astya965@gmail.com>
2020-06-11 22:19:09 +0300

    fix(2-1): remove old props from App

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+import {offerShape} from "../../utils/prop-types.js";
 import Main from "../main/main.jsx";
 
 const App = (props) => {
-  const {offers, placeTitles} = props;
+  const {offers} = props;
 
-  return <Main offers={offers} placeTitles={placeTitles}/>;
+  return <Main offers={offers}/>;
 
 };
 
 export default App;
+
+App.propTypes = {
+  offers: PropTypes.arrayOf(PropTypes.shape(offerShape)),
+};
