9ea8f614e87f223b4040c0df028bd2ea0df56138
astya965 <astya965@gmail.com>
2020-06-11 21:45:04 +0300

    feat(2-1, mocks): add offer mocks

--- src/mocks/offer.js
+++ src/mocks/offer.js
@@ -1,3 +1,34 @@
+import shortid from "shortid";
+import {getRandomElement} from "../utils/utils.js";
+
 const OFFERS_COUNT = 4;
 
-export {OFFERS_COUNT};
+const PLACE_TITLES = [
+  `Beautiful & luxurious apartment at great location`,
+  `Wood and stone place`,
+  `Canal View Prinsengracht`,
+  `Nice, cozy, warm big bed apartment`];
+
+/**
+ * Генерация моков для предложения
+ * @return {Object} Данные для предложения
+ */
+const genrateOffer = () => {
+  return {
+    id: shortid.generate(),
+    title: getRandomElement(PLACE_TITLES),
+  }
+}
+
+/**
+ * Генерация массива предложений
+ * @param {Number} count - Нужное число точек
+ * @return {Array} Массив предложений
+ */
+const generateOffers = (count) => {
+  return new Array(count)
+  .fill(null)
+  .map(genrateOffer);
+};
+
+export const offers = generateOffers(OFFERS_COUNT);
