24f898ae4959b54904ce482ae22ef16db4714aac
astya965 <astya965@gmail.com>
2020-06-14 19:22:14 +0300

    feat(2-2, configuration): configure Jest and replace Enzyme adapter to jest.setup.js

--- src/components/place-card/place-card.e2.test.js
+++ src/components/place-card/place-card.e2.test.js
@@ -1,12 +1,7 @@
 import React from "react";
-import Enzyme, {shallow} from "enzyme";
-import Adapter from "enzyme-adapter-react-16";
+import {shallow} from "enzyme";
 
 import PlaceCard from "./place-card.jsx";
-
-Enzyme.configure({
-  adapter: new Adapter()
-});
 
 const testOffer = {
   id: `1`,
