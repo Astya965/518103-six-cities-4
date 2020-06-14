2bdf19efee8869dadb690c9e2b7fdddd0b12f59a
astya965 <astya965@gmail.com>
2020-06-14 16:45:05 +0300

    test(2-2): add snapshot-test for PlaceCard component

---
+++ src/components/place-card/place-card.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import PlaceCard from "./place-card.jsx";
+
+const testOffer = {
+  id: `1`,
+  title: `Test name-1`,
+};
+
+it(`Render PlaceCard`, () => {
+  const tree = renderer.
+    create(
+        <PlaceCard offer={testOffer} />
+    ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
