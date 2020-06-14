f0443b91a0b545558d665a1f8597fd2111c6c7e4
astya965 <astya965@gmail.com>
2020-06-14 16:33:30 +0300

    test(2-2): add snapshot-test for Main component

---
+++ src/components/main/main.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Main from "./main.jsx";
+
+const testOffers = [
+  {
+    id: `1`,
+    title: `Test name-1`,
+  },
+  {
+    id: `2`,
+    title: `Test name-2`
+  }
+];
+
+it(`Render Main`, () => {
+  const tree = renderer.
+    create(
+        <Main offers={testOffers} />
+    ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
