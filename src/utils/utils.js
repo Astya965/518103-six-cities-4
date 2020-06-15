792bacdb30977ffad9dc90af1bdfd1db77206a19
astya965 <astya965@gmail.com>
2020-06-15 22:16:27 +0300

    feat(3-1): add getRandomNumber and getRandomBoolean to utils

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,4 +1,14 @@
 /**
+ * Выбор случайного числа в заданном промежутке
+ * @param {Number} min - Минимальное допустимое значение (включительно)
+ * @param {Number} max - Максимальное допустимое значение (включительно)
+ * @return {Number} Случайное целое число в данном промежутке
+ */
+export const getRandomNumber = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+/**
  * Выбор случайного элемента массива
  * @param {Array} array
  * @return {Any} Случайный элемент массива
@@ -6,4 +16,13 @@
 export const getRandomElement = (array) => {
   const randomIndex = Math.floor(Math.random() * (array.length));
   return array[randomIndex];
+};
+
+/**
+ * Генерация случайного значения true\false
+ * @return {Boolean}
+ */
+export const getRandomBoolean = () => {
+  const boolean = (Math.floor(Math.random() < 0.5)) ? true : false;
+  return boolean;
 };
