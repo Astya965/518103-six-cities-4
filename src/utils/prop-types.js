0e8d25a7580dacbff89146e728e7547d67521590
astya965 <astya965@gmail.com>
2020-06-15 22:20:08 +0300

    feat(3-1): update offerShape to prop-types.js

--- src/utils/prop-types.js
+++ src/utils/prop-types.js
@@ -2,5 +2,11 @@ import PropTypes from "prop-types";
 
 export const offerShape = {
   id: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired
+  isPremium: PropTypes.bool.isRequired,
+  isFavourite: PropTypes.bool.isRequired,
+  previewImage: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  rating: PropTypes.number.isRequired,
+  type: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
 };
