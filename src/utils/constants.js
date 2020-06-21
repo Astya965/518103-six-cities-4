5e3b42720aacb395059ce5571296d963abef4122
astya965 <astya965@gmail.com>
2020-06-21 15:28:31 +0300

    feat(3-2, mocks): add offerGoodsMap to constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,16 @@ export const offerTypesMap = {
   house: `House`,
   hotel: `Hotel`
 };
+
+export const offerGoodsMap = {
+  wifi: `Wi-Fi`,
+  washingMachine: `Washing machine`,
+  towels: `Towels`,
+  heating: `Heating`,
+  coffee: `Coffee machine`,
+  babySeat: `Baby seat`,
+  kitchen: `Kitchen`,
+  dishwasher: `Dishwasher`,
+  cabelTV: `Cabel TV`,
+  fridge: `Fridge`
+};
