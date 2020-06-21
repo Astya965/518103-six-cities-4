7a7511d5252c7cd79ac06dee846e31e40cbcb13f
astya965 <astya965@gmail.com>
2020-06-21 17:32:12 +0300

    feat(3-2): change App to a class compoment

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
@@ -6,27 +6,62 @@ import {offerShape} from "../../utils/prop-types.js";
 import Main from "../main/main.jsx";
 import PlaceDetails from "../place-details/place-details.jsx";
 
-const placeHeaderClickHandler = () => {};
+class App extends PureComponent {
+  constructor(props) {
+    super(props);
 
-const App = (props) => {
-  const {offers} = props;
+    this.state = {
+      currentOffer: null
+    };
 
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <Main
-            offers={offers}
-            placeHeaderClickHandler={placeHeaderClickHandler} />
-        </Route>
-        <Route exact path="/dev-component">
-          <PlaceDetails offer={offers[0]} />
-        </Route>
-      </Switch>
-    </Router>
-  );
+    this.placeHeaderClickHandler = this.placeHeaderClickHandler.bind(this);
+  }
 
-};
+  placeHeaderClickHandler(offer) {
+    this.setState({
+      currentOffer: offer
+    });
+  }
+
+  _renderMain() {
+    const {offers} = this.props;
+
+    return <Main offers={offers}
+      placeHeaderClickHandler={this.placeHeaderClickHandler} />;
+  }
+
+  _renderPlaceDetails() {
+    if (!this.state.currentOffer) {
+      return this._renderMain();
+    }
+
+    return <PlaceDetails offer={this.state.currentOffer} />;
+  }
+
+  _renderScreen() {
+    if (this.state.currentOffer) {
+      return (
+        this._renderPlaceDetails()
+      );
+    }
+    return this._renderMain();
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            {this._renderScreen()}
+          </Route>
+          <Route exact path="/dev-component">
+            {this._renderPlaceDetails()}
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
 
 App.propTypes = {
   offers: PropTypes.arrayOf(PropTypes.shape(offerShape)).isRequired,
