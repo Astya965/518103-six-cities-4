266c4f388ab742b2e685e4a514f18e1be55e051e
astya965 <astya965@gmail.com>
2020-06-22 15:50:14 +0300

    feat(3-2): add new components to PlaceDetails

--- src/components/place-details/place-details.jsx
+++ src/components/place-details/place-details.jsx
@@ -1,98 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import PlaceGallery from "../place-gallery/place-gallery.jsx";
+import PlaceInfo from "../place-info/place-info.jsx";
 import ReviewsList from "../reviews-list/reviews-list.jsx";
 import PlacesMap from "../places-map/places-map.jsx";
 import NearPlaces from "../near-places/near-places.jsx";
-import HostInfo from "../host-info/host-info.jsx";
 
-import {getRatingPercentage} from "../../utils/utils.js";
 import {offerShape} from "../../utils/prop-types.js";
 
 const PlaceDetails = (props) => {
   const {offer} = props;
-  const {isPremium, isFavourite, price, rating, title, type,
-    images, bedrooms, maxAdults, goods, description, host} = offer;
-
-  const favouriteClass = isFavourite ? `property__bookmark-button--active` : ``;
 
   return (
     <main className="page__main page__main--property">
       <section className="property">
-        <div className="property__gallery-container container">
-          <div className="property__gallery">
-            {images.map((image, i) => {
-              return (
-                <div className="property__image-wrapper" key={`image-${i}`}>
-                  <img className="property__image" src={image} alt="Offer photo" />
-                </div>
-              );
-            })}
-          </div>
-        </div>
+        <PlaceGallery images={offer.images} />
         <div className="property__container container">
           <div className="property__wrapper">
-            {isPremium ?
-              <div className="property__mark">
-                <span>Premium</span>
-              </div> : ``}
-            <div className="property__name-wrapper">
-              <h1 className="property__name">
-                {title}
-              </h1>
-              <button className={`property__bookmark-button button ${favouriteClass}`} type="button">
-                <svg className="property__bookmark-icon" width="31" height="33">
-                  <use xlinkHref="#icon-bookmark"></use>
-                </svg>
-                <span className="visually-hidden">To bookmarks</span>
-              </button>
-            </div>
-            <div className="property__rating rating">
-              <div className="property__stars rating__stars">
-                <span style={{width: getRatingPercentage(rating)}}></span>
-                <span className="visually-hidden">Rating</span>
-              </div>
-              <span className="property__rating-value rating__value">{rating}</span>
-            </div>
-            <ul className="property__features">
-              <li className="property__feature property__feature--entire">
-                {type}
-              </li>
-              <li className="property__feature property__feature--bedrooms">
-                {bedrooms} Bedrooms
-              </li>
-              <li className="property__feature property__feature--adults">
-                Max {maxAdults} adults
-              </li>
-            </ul>
-            <div className="property__price">
-              <b className="property__price-value">&euro;{price}</b>
-              <span className="property__price-text">&nbsp;night</span>
-            </div>
-            <div className="property__inside">
-              <h2 className="property__inside-title">What&apos;s inside</h2>
-              <ul className="property__inside-list">
-                {goods.map((good, i) => {
-                  return (
-                    <li className="property__inside-item" key={`good-${i}`}>
-                      {good}
-                    </li>
-                  );
-                })}
-              </ul>
-            </div>
-            <div className="property__host">
-              <HostInfo host={host} />
-              <div className="property__description">
-                {description.map((paragraph, i) => {
-                  return (
-                    <p className="property__text" key={`paragraph-${i}`}>
-                      {paragraph}
-                    </p>
-                  );
-                })}
-              </div>
-            </div>
+            <PlaceInfo offer={offer}/>
             <ReviewsList />
           </div>
         </div>
