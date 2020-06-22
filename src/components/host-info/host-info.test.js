5af81de9ebab737bbe3b71f8439f961f65530a3a
astya965 <astya965@gmail.com>
2020-06-22 14:30:29 +0300

    style(3-2): fix typo

--- src/components/host-info/host-info.test.js
+++ src/components/host-info/host-info.test.js
@@ -4,7 +4,7 @@ import renderer from "react-test-renderer";
 import HostInfo from "./host-info.jsx";
 import {testOffer} from "../../mocks/test-mocks.js";
 
-it(`Render PlaceDetails`, () => {
+it(`Render HostInfo`, () => {
   const tree = renderer.
     create(
         <HostInfo host={testOffer.host} />
