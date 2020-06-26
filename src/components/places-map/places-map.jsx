6c078eefb2d8e97edacf8f0e039fd54f061f1421
astya965 <astya965@gmail.com>
2020-06-26 17:08:49 +0300

    feat(4-1): git add tileLayer to PlaceMap

--- src/components/places-map/places-map.jsx
+++ src/components/places-map/places-map.jsx
@@ -34,6 +34,10 @@ export default class PlacesMap extends Component {
 
     this._mapInstance = leaflet.map(this._mapRef.current, mapSettings);
     this._mapInstance.setView([latitude, longitude], zoom);
+
+    leaflet.tileLayer(`https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png`, {
+      attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
+    }).addTo(this._mapInstance);
   }
 
   componentDidMount() {
