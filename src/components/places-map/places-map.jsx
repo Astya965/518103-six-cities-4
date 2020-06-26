7d4422abbc94af40ef37f5df0ef8a5a5dde5169f
astya965 <astya965@gmail.com>
2020-06-26 13:37:07 +0300

    feat(4-1): add the PlacesMap component with ViewMode

--- src/components/places-map/places-map.jsx
+++ src/components/places-map/places-map.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const PlacesMap = () => {
+import {VIEWMODES, ViewMode} from "../../utils/constants.js";
+import {offerShape} from "../../utils/prop-types.js";
+
+const PlacesMap = (props) => {
+  const {viewMode} = props;
+  const isNearViewMode = viewMode === ViewMode.PlaceDetails;
 
   return (
-    <section className="property__map map"></section>
+    <section className={`${isNearViewMode ? `property__map` : `cities__map`} map`}>
+      <div id="map"></div>
+    </section>
   );
 };
+
+PlacesMap.propTypes = {
+  offers: PropTypes.arrayOf(PropTypes.shape(offerShape)).isRequired,
+  viewMode: PropTypes.oneOf(VIEWMODES).isRequired,
+}
 
 export default PlacesMap;
