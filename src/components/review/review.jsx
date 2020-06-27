c92712c19941f9e4aa19c213b36531ad56627889
astya965 <astya965@gmail.com>
2020-06-28 00:44:25 +0300

    feat(4-2): add props to the Review component

--- src/components/review/review.jsx
+++ src/components/review/review.jsx
@@ -1,31 +1,43 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Review = () => {
+import {reviewShape} from "../../utils/prop-types.js";
+import {getRatingPercentage} from "../../utils/utils.js";
+import {formatReverseDate, formatMonthDay} from "../../utils/date.js";
+
+const Review = (props) => {
+  const {review} = props;
+  const {comment, date, rating, user} = review;
+  const {avatarUrl, name} = user;
 
   return (
     <li className="reviews__item">
       <div className="reviews__user user">
         <div className="reviews__avatar-wrapper user__avatar-wrapper">
-          <img className="reviews__avatar user__avatar" src="img/avatar-max.jpg" width="54" height="54" alt="Reviews avatar" />
+          <img className="reviews__avatar user__avatar" src={avatarUrl} width="54" height="54" alt="Reviews avatar" />
         </div>
         <span className="reviews__user-name">
-          Max
+          {name}
         </span>
       </div>
       <div className="reviews__info">
         <div className="reviews__rating rating">
           <div className="reviews__stars rating__stars">
-            <span style={{width: `80%`}}></span>
+            <span style={{width: getRatingPercentage(rating)}}></span>
             <span className="visually-hidden">Rating</span>
           </div>
         </div>
         <p className="reviews__text">
-          A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
+          {comment}
         </p>
-        <time className="reviews__time" dateTime="2019-04-24">April 2019</time>
+        <time className="reviews__time" dateTime={formatReverseDate(date)}>{formatMonthDay(date)}</time>
       </div>
     </li>
   );
+};
+
+Review.propTypes = {
+  review: PropTypes.shape(reviewShape).isRequired
 };
 
 export default Review;
