5806bf02aeb52b0d19904f798e92c00ab6894994
astya965 <astya965@gmail.com>
2020-06-28 02:36:00 +0300

    fix(4-2): add reviews count to ReviewsList

--- src/components/reviews-list/reviews-list.jsx
+++ src/components/reviews-list/reviews-list.jsx
@@ -12,7 +12,7 @@ const ReviewsList = (props) => {
 
   return (
     <section className="property__reviews reviews">
-      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
+      <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
       <ul className="reviews__list">
         {reviews
           .slice(0, MAX_REVIEWS_COUNT)
