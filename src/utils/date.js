b142907763b17e5ea5fa08fb473593da3e1049e1
astya965 <astya965@gmail.com>
2020-06-28 02:26:18 +0300

    fix(4-2): fix formatMonthDay

--- src/utils/date.js
+++ src/utils/date.js
@@ -10,10 +10,11 @@ export const formatReverseDate = (date) => {
 };
 
 /**
- * Получение времени на основе даты в формате MMMM DD (полное название месяца и дата)
+ * Получение времени на основе даты в формате MMMM DD YYYY
+ * (полное название месяца и дата, год)
  * @param {Number} date - Дата в unix формате
- * @return {String} Дата события в формате MMMM DD
+ * @return {String} Дата события в формате MMMM DD YYYY
  */
 export const formatMonthDay = (date) => {
-  return moment(date).format(`MMMM DD`);
+  return moment(date).format(`MMMM DD, YYYY`);
 };
