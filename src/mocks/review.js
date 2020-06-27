658eaf8d21cda855d38d3d529a48115787a097a3
astya965 <astya965@gmail.com>
2020-06-27 23:39:58 +0300

    feat(4-2, mocks): add reviews mock

---
+++ src/mocks/review.js
@@ -0,0 +1,63 @@
+import {getRandomNumber, getRandomElement, getRandomBoolean} from "../utils/utils.js";
+import shortid from "shortid";
+
+const COMMENTS_COUNT = 3;
+
+/**
+ * 1483228800 = Jan 01 2017
+ */
+const MIN_DATE = 1483228800;
+
+const MAX_DATE = Date.now();
+
+const names = [`Anna`, `Lina`, `Simon`, `Alice`, `Mary`, `Max`, `Alex`];
+
+const avatars = [`img/avatar-angelina.jpg`, `img/avatar-max.jpg`];
+
+const comments = [`A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam.`,
+  `Good location next to Amsterdam Zuid and by tram to museumplein 5 min. Nice view from window`,
+  `Didn't like that the bathroom door inside the room is wide open.
+  Overall the hotel is really close to the metro/train station and it was clean and comfortable.`,
+  `The only problem was to find the place..everything else was great!`,
+  `Everything was fantastic. Location is excellent, very friendly staff, cleanliness flawless. I would definitely recommend this hotel to anyone.`,
+  `Beautiful cozy, room, this time on the 6th floor, great breakfast as always, exceptional service throughout...`];
+
+/**
+ * Генерация случайно даты в промежутке от MIN_DATE до MAX_DATE
+ * @return {Number} СЛучайная дата в unix формате
+ */
+const getRandomDate = () => {
+  return getRandomNumber(MIN_DATE, MAX_DATE);
+};
+
+/**
+ * Генерация моков для отзыва
+ * @return {Object} Данные для отзывов
+ */
+const genrateComment = () => {
+  return {
+    comment: getRandomElement(comments),
+    date: getRandomDate(),
+    id: shortid.generate(),
+    rating: getRandomNumber(0, 5),
+    user: {
+      avatarUrl: getRandomElement(avatars),
+      id: `host-${shortid.generate()}`,
+      isPro: getRandomBoolean(),
+      name: getRandomElement(names),
+    }
+  };
+};
+
+/**
+ * Генерация массива предложений
+ * @param {Number} count - Нужное число точек
+ * @return {Array} Массив предложений
+ */
+const generateComments = (count) => {
+  return new Array(count)
+  .fill(null)
+  .map(genrateComment);
+};
+
+export const reviews = generateComments(COMMENTS_COUNT);
