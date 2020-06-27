edfd33b1d49344cdc2f7a485ac11df1f0d10b5b1
astya965 <astya965@gmail.com>
2020-06-28 02:29:27 +0300

    test(4-2): update tests

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,12 +2,12 @@ import React from "react";
 import renderer from "react-test-renderer";
 
 import App from "./app.jsx";
-import {testOffers} from "../../mocks/test-mocks.js";
+import {testOffers, testReviews} from "../../mocks/test-mocks.js";
 
 it(`Render App`, () => {
   const tree = renderer.
     create(
-        <App offers={testOffers}/>
+        <App offers={testOffers} reviews={testReviews}/>
     ).toJSON();
 
   expect(tree).toMatchSnapshot();
--- src/components/reviews-list/reviews-list.test.js
+++ src/components/reviews-list/reviews-list.test.js
@@ -1,12 +1,14 @@
 import React from "react";
 import renderer from "react-test-renderer";
 
+import {testReviews} from "../../mocks/test-mocks.js";
+
 import ReviewsList from "./reviews-list.jsx";
 
 it(`Render ReviewsList`, () => {
   const tree = renderer.
     create(
-        <ReviewsList />
+        <ReviewsList reviews={testReviews} />
     ).toJSON();
 
   expect(tree).toMatchSnapshot();
