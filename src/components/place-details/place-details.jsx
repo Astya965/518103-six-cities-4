89eb05714dc2d084c696fdf08f226cb38c92cf97
astya965 <astya965@gmail.com>
2020-06-28 03:43:34 +0300

    feat(4-2): add limit nearOffers and offer images

--- src/components/place-details/place-details.jsx
+++ src/components/place-details/place-details.jsx
@@ -8,18 +8,19 @@ import PlacesMap from "../places-map/places-map.jsx";
 import NearPlaces from "../near-places/near-places.jsx";
 
 import {offerShape, reviewShape} from "../../utils/prop-types.js";
+import {MAX_NEAR_OFFERS_COUNT, MAX_IMAGES_COUNT, ViewMode} from "../../utils/constants.js";
 import {ViewMode} from "../../utils/constants.js";
 import {memoize} from "../../utils/utils.js";
 
 const PlaceDetails = (props) => {
-  const {offer, offers, reviews} = props;
-  const filterOffers = memoize((id) => offers.filter((item) => item.id !== id));
+  const {offer, offers, reviews, placeHeaderClickHandler} = props;
+  const filterOffers = memoize((id) => offers.filter((item) => item.id !== id).slice(0, MAX_NEAR_OFFERS_COUNT));
   const nearOffers = filterOffers(offer.id);
 
   return (
     <main className="page__main page__main--property">
       <section className="property">
-        <PlaceGallery images={offer.images} />
+        <PlaceGallery images={offer.images.slice(0, MAX_IMAGES_COUNT)} />
         <div className="property__container container">
           <div className="property__wrapper">
             <PlaceInfo offer={offer}/>
@@ -28,7 +29,7 @@ const PlaceDetails = (props) => {
         </div>
         <PlacesMap offers={nearOffers} viewMode={ViewMode.PlaceDetails}/>
       </section>
-      <NearPlaces />
+      <NearPlaces offers={nearOffers} viewMode={ViewMode.PlaceDetails} placeHeaderClickHandler={placeHeaderClickHandler}/>
     </main>
   );
 };
@@ -37,6 +38,7 @@ PlaceDetails.propTypes = {
   offers: PropTypes.arrayOf(PropTypes.shape(offerShape)).isRequired,
   offer: PropTypes.shape(offerShape).isRequired,
   reviews: PropTypes.arrayOf(PropTypes.shape(reviewShape)).isRequired,
+  placeHeaderClickHandler: PropTypes.func.isRequired,
 };
 
 export default PlaceDetails;
