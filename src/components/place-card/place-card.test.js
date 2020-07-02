4c635406e9352d28df63ca61c119041f7c51fe3c
astya965 <astya965@gmail.com>
2020-07-02 13:20:58 +0300

    test(4-2): add test cases for PlaceCard

--- src/components/place-card/place-card.test.js
+++ src/components/place-card/place-card.test.js
@@ -4,16 +4,33 @@ import renderer from "react-test-renderer";
 import PlaceCard from "./place-card.jsx";
 import {testOffer} from "../../mocks/test-mocks.js";
 
-it(`Render PlaceCard`, () => {
-  const tree = renderer.
-    create(
-        <PlaceCard offer={testOffer}
-          isMainView={true}
-          onPlaceHeaderClick={() => {}}
-          handleCardPointerEnter = {() => {}}
-          handleCardPointerLeave = {() => {}}
-        />
-    ).toJSON();
+describe(`Render PlaceCard`, () => {
+  it(`Render PlaceCard with isMainView=true`, () => {
+    const tree = renderer.
+      create(
+          <PlaceCard offer={testOffer}
+            isMainView={true}
+            onPlaceHeaderClick={() => {}}
+            handleCardPointerEnter = {() => {}}
+            handleCardPointerLeave = {() => {}}
+          />
+      ).toJSON();
 
-  expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it(`Render PlaceCard isMainView=false`, () => {
+    const tree = renderer.
+      create(
+          <PlaceCard offer={testOffer}
+            isMainView={false}
+            onPlaceHeaderClick={() => {}}
+            handleCardPointerEnter = {() => {}}
+            handleCardPointerLeave = {() => {}}
+          />
+      ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
 });
