63130bb9c516a9d06a8d9efb486b6caf08d650cc
astya965 <astya965@gmail.com>
2020-07-04 13:27:00 +0000

    fix(4-2); change removing markers method

--- src/components/places-map/places-map.jsx
+++ src/components/places-map/places-map.jsx
@@ -12,7 +12,7 @@ export default class PlacesMap extends Component {
     this._mapRef = createRef();
     this._mapInstance = null;
 
-    this._markers = [];
+    this._markers = {};
   }
 
   _initMap() {
@@ -36,27 +36,27 @@ export default class PlacesMap extends Component {
       attribution: `&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>`
     }).addTo(this._mapInstance);
 
-    this._addMarkers(this.props.offers);
+    this._addMarkers();
   }
 
-  _addMarkers(offers) {
+  _addMarkers(offers = this.props.offers) {
     const {activeOffer} = this.props;
 
     if (this._mapInstance) {
       offers.forEach((offer) => {
-        this._addMarker(offer.location.latitude, offer.location.longitude, false);
+        this._addMarker(offer.location.latitude, offer.location.longitude, false, offer.id);
       });
-    }
 
-    if (this._mapInstance && activeOffer !== null) {
-      this._addMarker(activeOffer.location.latitude, activeOffer.location.longitude, true);
+      if (activeOffer !== null) {
+        this._addMarker(activeOffer.location.latitude, activeOffer.location.longitude, true, activeOffer.id);
+      }
     }
   }
 
-  _addMarker(latitude, longitude, isActive) {
+  _addMarker(latitude, longitude, isActive, id) {
     const icon = this._getMarkerTemplate(isActive);
     const marker = leaflet.marker([latitude, longitude], {icon}).addTo(this._mapInstance);
-    this._markers.push(marker);
+    this._markers[id] = marker;
   }
 
   _getMarkerTemplate(isActive) {
@@ -66,13 +66,12 @@ export default class PlacesMap extends Component {
     });
   }
 
-  _clearMarkers() {
+  _clearMarkers(markers) {
     if (this._mapInstance !== null) {
-      this._markers.forEach((marker) => {
-        this._mapInstance.removeLayer(marker);
+      markers.forEach((marker) => {
+        this._mapInstance.removeLayer(this._markers[marker.id]);
       });
     }
-    this._markers = [];
   }
 
   componentDidMount() {
@@ -84,12 +83,16 @@ export default class PlacesMap extends Component {
   }
 
   componentDidUpdate(prevProps) {
-    const {activeOffer: prevActiveOffer} = prevProps;
-    const {activeOffer} = this.props;
+    const {activeOffer: prevActiveOffer, offers: prevOffers} = prevProps;
+    const {activeOffer, offers} = this.props;
+
+    const newMarkers = offers.filter((offer) => !prevOffers.includes(offer));
+    const outdatedMarkers =  prevOffers.filter((offer) => !offers.includes(offer));
+    outdatedMarkers.push(prevActiveOffer);
 
     if (prevActiveOffer.id !== activeOffer.id) {
-      this._clearMarkers();
-      this._addMarkers(this.props.offers);
+      this._clearMarkers(outdatedMarkers);
+      this._addMarkers(newMarkers);
     }
   }
 
