c5b556d72177cae9c950b4292af0bde1c5a04eed
astya965 <astya965@gmail.com>
2020-07-04 10:54:39 +0000

    test(4-2): fix e2e tests

--- src/utils/date.unit.test.js
+++ src/utils/date.unit.test.js
@@ -1,18 +1,12 @@
 import {formatReverseDate, formatMonthDay} from "./date.js";
 
-const testDate = {
-  unix: 1593807294000,
-  revrse: `2020-07-03`,
-  monthDay: `July 03, 2020`,
-};
-
 describe((`date formatting tests`), () => {
 
   it(`formatReverseDate test`, () => {
-    expect(formatReverseDate(testDate.unix)).toBe(testDate.revrse);
+    expect(formatReverseDate(1593807294000)).toBe(`2020-07-03`);
   });
 
   it(`formatMonthDay test`, () => {
-    expect(formatMonthDay(testDate.unix)).toBe(testDate.monthDay);
+    expect(formatMonthDay(unix1593807294000)).toBe(`July 03, 2020`);
   });
 });
