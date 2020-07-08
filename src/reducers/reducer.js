447e3486688d0edbab1eb296e8df19ff9a2a31d9
astya965 <astya965@gmail.com>
2020-07-08 13:26:31 +0000

    feat(5-1, reducer): add ActionType to reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -7,3 +7,12 @@ const initialState = {
   nearOffers: [],
   reviews: []
 };
+
+export const ActionType = {
+  SET_CURRENT_CITY: `SET_CURRENT_CITY`,
+  SET_CURRENT_OFFER: `SET_CURRENT_OFFER`,
+  LOAD_OFFERS: `LOAD_OFFERS`,
+  SET_CITIES: `SET_CITIES`,
+  LOAD_REVIEWS: `LOAD_REVIEWS`,
+  LOAD_NEAR_OFFERS: `LOAD_NEAR_OFFERS`,
+};
