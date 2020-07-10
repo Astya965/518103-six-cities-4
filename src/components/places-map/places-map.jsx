7df84b5438e0e45fd4c66fac51688ba15ab0654c
astya965 <astya965@gmail.com>
2020-07-10 15:20:32 +0000

    fix(4-2): fix PlacesMap

--- src/components/places-map/places-map.jsx
+++ src/components/places-map/places-map.jsx
@@ -54,10 +54,6 @@ export default class PlacesMap extends Component {
 
       if (activeOffer !== null) {
         this._addMarker(activeOffer.location.latitude, activeOffer.location.longitude, true, activeOffer.id);
-
-        if (!this._offersMap.has(activeOffer)) {
-          this._offersMap.add(activeOffer);
-        }
       }
     }
   }
@@ -80,32 +76,32 @@ export default class PlacesMap extends Component {
       offers.forEach((offer) => {
         this._mapInstance.removeLayer(this._markers.get(offer.id));
         this._markers.delete(offer.id);
+        this._offersMap.delete(offer);
       })
     }
   }
 
-  _compareMarkers(newOffers) {
+  _compareMarkers(newData) {
     const newOffersSet = new Set();
-    const outdatedOffersSet = new Set();
+    const newOffers = [];
+    const outdatedOffers = [];
 
-    newOffers.forEach((offer) => {
+    newData.forEach((offer) => {
+      newOffersSet.add(offer);
       if (!this._offersMap.has(offer)) {
-        newOffersSet.add(offer);
+        newOffers.push(offer);
       }
     });
 
-    this._offersMap.forEach((value) => {
-      if (!newOffersSet.has(value)) {
-        outdatedOffersSet.add(value);
+    this._offersMap.forEach((offer) => {
+      if (!newOffersSet.has(offer)) {
+        outdatedOffers.push(offer);
       }
     });
-
-    console.log(newOffersSet);
-    console.log(outdatedOffersSet);
 
     return {
-      newOffersSet,
-      outdatedOffersSet
+      newOffers,
+      outdatedOffers
     }
   }
 
@@ -118,14 +114,15 @@ export default class PlacesMap extends Component {
   }
 
   componentDidUpdate(prevProps) {
-    const {activeOffer: prevActiveOffer, offer: prevOffers} = prevProps;
+    const {activeOffer: prevActiveOffer} = prevProps;
     const {activeOffer, offers} = this.props;
 
     if (prevActiveOffer.id !== activeOffer.id) {
-      const {newOffersSet, outdatedOffersSet} = this._compareMarkers(offers, prevOffers);
+      const {newOffers, outdatedOffers} = this._compareMarkers(offers);
 
-      this._clearMarkers(outdatedOffersSet);
-      this._addMarkers(newOffersSet);
+      outdatedOffers.push(prevActiveOffer);
+      this._clearMarkers(outdatedOffers);
+      this._addMarkers(newOffers);
     }
   }
 
