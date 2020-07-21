313e71e6890d083f6281c800f26d1a74659f4a93
astya965 <astya965@gmail.com>
2020-07-21 21:05:50 +0000

    fix(5-1): fix nearby selector

--- src/store/nearby/selector.js
+++ src/store/nearby/selector.js
@@ -1,13 +1,13 @@
 import {createSelector} from "reselect";
-import NameSpace from "../name-space.js";
 
 import {MAX_NEARBY_OFFERS_COUNT} from "../../utils/constants.js";
-
-const getAllNearby = (state) => (state[NameSpace.REVIEWS].reviews);
+import {getCurrentOffers} from "../offers/selector.js";
+import {getCurrentOffer} from "../cities/selector.js";
 
 export const getNearby = createSelector(
-    getAllNearby,
-    (nearby) => {
-      return nearby.slice(0, MAX_NEARBY_OFFERS_COUNT);
+    getCurrentOffers,
+    getCurrentOffer,
+    (offers, offer) => {
+      return offers.filter((item) => item.id !== offer.id).slice(0, MAX_NEARBY_OFFERS_COUNT);
     }
 );
